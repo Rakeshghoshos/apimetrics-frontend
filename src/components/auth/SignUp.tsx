29702c0c2458f2a3b6f2c0b110b31b4df4535fc4
Rakesh Ghosh <142241392+Rakeshghoshos@users.noreply.github.com>
2024-09-19 18:07:55 +0530

    Update SignUp.tsx

--- src/components/auth/SignUp.tsx
+++ src/components/auth/SignUp.tsx
@@ -43,11 +43,11 @@ const Signup: React.FC = () => {
     <>
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <img
+{/*           <img
             alt="Your Company"
             src=""
             className="mx-auto h-10 w-auto"
-          />
+          /> */}
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
             Create a new account
           </h2>
