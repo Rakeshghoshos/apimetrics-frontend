9f44437c55d3c2d0e933c8d8197a23d7a4a24aae
Rakesh Ghosh <142241392+Rakeshghoshos@users.noreply.github.com>
2024-09-19 18:05:45 +0530

    Update Login.tsx

--- src/components/auth/Login.tsx
+++ src/components/auth/Login.tsx
@@ -42,7 +42,7 @@ export default function Login() {
             <div className="sm:mx-auto sm:w-full sm:max-w-sm">
               <img
                 alt="Your Company"
-                src=""
+                src="https://images.app.goo.gl/qxMvaTdLSZDAVMFP7"
                 className="mx-auto h-10 w-auto"
               />
               <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
