52551653b1853d4a33bd797a27c93166707512a1
Rakesh Ghosh <142241392+Rakeshghoshos@users.noreply.github.com>
2024-09-20 11:24:55 +0530

    Update UserSlice.tsx

--- src/components/auth/UserSlice.tsx
+++ src/components/auth/UserSlice.tsx
@@ -1,17 +1,17 @@
-// UserSlice.ts
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit'
+import { createAsyncThunk } from "@reduxjs/toolkit";
 import user from './UserApi';
 
-type userState = {
-  user: any,
-  error: any,
+type userState= {
+  user:any,
+  error:any,
   status: string | null
 }
 
-type attributes = {
-  emailId: string,
-  password: string,
-  token?: string
+type attributes={
+  emailId:string,
+  password:string,
+    token?:string
 }
 
 const initialState: userState = {
@@ -23,43 +23,62 @@ const initialState: userState = {
 type getUserResponse = Awaited<ReturnType<typeof user.loginApi>>;
 
 export const fetchUserAsync = createAsyncThunk<
-  getUserResponse,
+getUserResponse,
   attributes,
   { rejectValue: string }
 >('user/fetchUser', async (values, { rejectWithValue }) => {
   try {
-    const response = await user.loginApi(values);
-    return response.data;
+      const response = await user.loginApi(values);
+      return response.data;
   } catch (error: any) {
-    return rejectWithValue('Invalid email or password');
+      return rejectWithValue(error.message);
+  }
+});
+
+export const signUpUserAsync = createAsyncThunk<
+getUserResponse,
+  attributes,
+  { rejectValue: string }
+>('user/signUpUser', async (values, { rejectWithValue }) => {
+  try {
+      const response = await user.signUpApi(values);
+      return response.data;
+  } catch (error: any) {
+      return rejectWithValue(error.message);
   }
 });
 
 export const userSlice = createSlice({
   name: 'user',
   initialState,
-  reducers: {
-    clearError(state) {
-      state.error = null;
-    }
-  },
+  reducers: {},
   extraReducers: (builder) => {
     builder
-      .addCase(fetchUserAsync.pending, (state) => {
-        state.status = 'loading';
+        .addCase(fetchUserAsync.pending, (state) => {
+            state.status = 'loading';
+        })
+        .addCase(fetchUserAsync.fulfilled, (state, action) => {
+            state.status = 'succeeded';
+            state.user = action.payload;
+        })
+        .addCase(fetchUserAsync.rejected, (state, action) => {
+            state.status = 'failed';
+            state.error = action.payload;
+        })
+        .addCase(signUpUserAsync.pending, (state) => {
+          state.status = 'loading';
       })
-      .addCase(fetchUserAsync.fulfilled, (state, action) => {
-        state.status = 'succeeded';
-        state.user = action.payload;
-        state.error = null;
+      .addCase(signUpUserAsync.fulfilled, (state, action) => {
+          state.status = 'succeeded';
+          state.user = action.payload;
       })
-      .addCase(fetchUserAsync.rejected, (state, action) => {
-        state.status = 'failed';
-        state.error = action.payload;
-      });
-  },
-});
+      .addCase(signUpUserAsync.rejected, (state, action) => {
+          state.status = 'failed';
+          state.error = action.payload;
+      })
+},
+})
 
-export const { clearError } = userSlice.actions;
+export const { } = userSlice.actions
 
-export default userSlice.reducer;
+export default userSlice.reducer
