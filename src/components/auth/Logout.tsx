2785d5f85434a9d6f276751418022adcbeffc765
Rakesh Ghosh <142241392+Rakeshghoshos@users.noreply.github.com>
2024-09-21 21:50:04 -0700

    Update Logout.tsx

--- src/components/auth/Logout.tsx
+++ src/components/auth/Logout.tsx
@@ -1,14 +1,17 @@
 import React,{useEffect} from 'react';
 import { useNavigate } from 'react-router-dom';
 import Storage from '../../core/storage';
+import { logout } from './UserSlice';
+import { useAppDispatch } from '../../core/Store';
 
 
 const Logout = () => {
 
     const navigate = useNavigate();
-
+  const dispatch = useAppDispatch();
     useEffect(() => {
       Storage.removeAll();
+      dispatch(logout());
       navigate('/'); 
     }, [])
     
